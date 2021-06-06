e0322eb59cba08c8ff7e38f033e576a0ded91589
Pedro Antunes <pedronmantunes@gmail.com>
2021-06-06 01:40:56 +0100

    merged main.cpp to master from branch mbed_01

--- main.cpp
+++ main.cpp
@@ -3,11 +3,6 @@
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 
-Thread thread_msg;
-
-char counter = 0;
-
-
 float calculateR0(AnalogIn s, float ratio) {
         float sensor_volt;
         float rs;
@@ -43,29 +38,24 @@ char determinePPM(AnalogIn sensor, float R0, float m, float b) {
         return (char)floor(ppm);
 }
 
-void air_measure(void){
+void sensor_read(){
   unsigned char buffer[8];
   float *f_buf = (float*)(buffer+1);
 
   while(1){
-    buffer[0] = AIR;
-    *f_buf = determinePPM(sensorMQ2, r0MQ2, slopeMQ2, interceptMQ2); //mq2sensorPPM;
+    buffer[0] = LUM;
+    *f_buf = (float)pot1.read()*100; //mq2sensorPPM;
     can_mutex.lock();
-    can1.write(CANMessage(1337, buffer, 5));
+    mail_t *mail = mail_box.try_alloc();
+    mail->identifier = buffer[0];
+    mail->data = *f_buf;
+    mail_box.put(mail);
     can_mutex.unlock();
     stdio_mutex.lock();
-    printf("Sent air: %d\n", (int)*f_buf);
+    printf("Sent luminosity: %d\n", (int)*f_buf);
     stdio_mutex.unlock();
-    ThisThread::sleep_for(1s);
-  }
+    ThisThread::sleep_for(SENSOR_INTERVAL);
 
-}
-
-void temperature_measure(void){
-  unsigned char buffer[8];
-  float *f_buf = (float*)(buffer+1);
-
-  while(1){
     buffer[0] = TMP;
     //Try to open the LM75B
     if (sensor.open()) {
@@ -73,11 +63,12 @@ void temperature_measure(void){
         printf("Device detected!\n");
         stdio_mutex.unlock();
         *f_buf=sensor.temp();
-        stdio_mutex.lock();
-        printf("Temperature calculated: %d\n", (int)*f_buf);
-        stdio_mutex.unlock();
         can_mutex.lock();
-        can1.write(CANMessage(1337, buffer, sizeof(buffer)));
+        if(can.write(CANMessage(1337, buffer, sizeof(buffer)))){
+          stdio_mutex.lock();
+          printf("Sent temperature: %d\n", (int)*f_buf);
+          stdio_mutex.unlock();
+        }
         can_mutex.unlock();
 
     } else {
@@ -86,30 +77,28 @@ void temperature_measure(void){
         stdio_mutex.unlock();
         buffer[1] = ERR;
         can_mutex.lock();
-        can1.write(CANMessage(1337, buffer, 2));
+        can.write(CANMessage(1337, buffer, 2));
         can_mutex.unlock();
     }
-    ThisThread::sleep_for(5s);
-  }
-}
+    ThisThread::sleep_for(SENSOR_INTERVAL);
 
-void send(void){
-  while(1){
-    stdio_mutex.lock();
-    printf("send()\n");
-    if (can1.write(CANMessage(1337, &counter, 1))) {
-      printf("wloop()\n");
-      counter++;
-      printf("Message sent: %d\n", counter);
+    buffer[0] = AIR;
+    *f_buf = determinePPM(sensorMQ2, r0MQ2, slopeMQ2, interceptMQ2); //mq2sensorPPM;
+    can_mutex.lock();
+    if(can.write(CANMessage(1337, buffer, 5))){
+      stdio_mutex.lock();
+      printf("Sent air: %d\n", (int)*f_buf);
+      stdio_mutex.unlock();
     }
-    stdio_mutex.unlock();
-    ThisThread::sleep_for(1s);
+    can_mutex.unlock();
+    ThisThread::sleep_for(SENSOR_INTERVAL);
   }
 }
 
 void process_msg(void){
   char msg_aux;
   float f_msg_aux;
+  char buffer[8];
 
   while(1){
     mail_t *mail = mail_box.try_get_for(100ms);
@@ -125,13 +114,59 @@ void process_msg(void){
         lcd.locate(0,0);
         lcd.printf("Air quality: %d\n", (int)f_msg_aux);
         lcd_mutex.unlock();
+        if(f_msg_aux>50){
+          buffer[0]=INIT;
+          buffer[1]=SOS;
+          buffer[2]=END;
+          node.write(buffer, sizeof(buffer));
+        }
+        ThisThread::sleep_for(10ms);
+        buffer[0]=INIT;
+        buffer[1]=AIR;
+        buffer[2] = (char)f_msg_aux;
+        buffer[3]=END;
+        node.write(buffer, sizeof(buffer));
+        ThisThread::sleep_for(10ms);
       } else if(msg_aux==TMP){
         f_msg_aux = mail->data;
         lcd_mutex.lock();
         lcd.cls();
-        lcd.locate(0,12);
+        lcd.locate(0,5);
         lcd.printf("Temperature: %d\n", (int)f_msg_aux);
         lcd_mutex.unlock();
+        if(f_msg_aux>50){
+          buffer[0]=INIT;
+          buffer[1]=SOS;
+          buffer[2]=END;
+          node.write(buffer, sizeof(buffer));
+        }
+        ThisThread::sleep_for(10ms);
+        buffer[0]=INIT;
+        buffer[1]=TMP;
+        buffer[2] = (char)f_msg_aux;
+        buffer[3]=END;
+        node.write(buffer, sizeof(buffer));
+        ThisThread::sleep_for(10ms);
+      } else if(msg_aux==LUM){
+        f_msg_aux = mail->data;
+        lcd_mutex.lock();
+        lcd.cls();
+        lcd.locate(0,10);
+        lcd.printf("Luminosity: %d\n", (int)f_msg_aux);
+        lcd_mutex.unlock();
+        if(f_msg_aux>50){
+          buffer[0]=INIT;
+          buffer[1]=SOS;
+          buffer[2]=END;
+          node.write(buffer, sizeof(buffer));
+        }
+        ThisThread::sleep_for(10ms);
+        buffer[0]=INIT;
+        buffer[1]=LUM;
+        buffer[2] = (char)f_msg_aux;
+        buffer[3]=END;
+        node.write(buffer, sizeof(buffer));
+        ThisThread::sleep_for(10ms);
       }
       mail_box.free(mail);
     }
@@ -142,21 +177,22 @@ int main(){
   //Uncomment if we want to reset R0 from default to our environment
   //r0MQ2 = calculateR0(sensorMQ2, airRatioMQ2);
 
-  printf("main()\n");
+/*
+  node.write("Hi Node!\n", 4);
+  if (node.read(buf, sizeof(buf))) {
+    printf("Node answered back :)\n");
+  }*/
+  printf("Entering main()\n");
 
-  /*thread.start(send);
-  thread.set_priority(osPriorityHigh);
-  thread_air.start(air_measure);
-  thread_air.set_priority(osPriorityNormal);*/
-  thread_temprature.start(temperature_measure);
-  thread_temprature.set_priority(osPriorityNormal);
+  thread_sensor_read.start(callback(sensor_read));
+  thread_sensor_read.set_priority(osPriorityNormal);
   thread_msg.start(callback(process_msg));
-  thread_msg.set_priority(osPriorityNormal1);
+  thread_msg.set_priority(osPriorityNormal2);
 
   CANMessage msg;
 
   while (1) {
-    if (can2.read(msg)) {
+    if (can.read(msg)) {
       mail_t *mail = mail_box.try_alloc();
       mail->identifier = msg.data[0];
       float *f_buf = (float*)(msg.data+1);
